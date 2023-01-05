5e40c0dd0d2766e2b055250690f24e66ec7e48b4
kolashah <105250729+kolashah@users.noreply.github.com>
2023-01-04 19:23:47 -0500

    frontend skeleton set up

--- webpack.config.js
+++ webpack.config.js
@@ -50,8 +50,9 @@ module.exports = {
         use: ['source-map-loader'],
       },
       {
-        test: /\.(scss|sass)$/,
+        test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: [/node_modules/],
       },
       {
         test: /\.module\.css$/,
@@ -61,6 +62,14 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [require('autoprefixer')],
+              },
             },
           },
         ],
